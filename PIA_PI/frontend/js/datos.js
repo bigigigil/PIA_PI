9d9fe3cc08751154087537a5ffae0a0c134916ef
Roxanna Abigail <rati.bigigigil@gmail.com>
2026-03-04 00:13:47 -0600

    banderas + clips

---
+++ PIA_PI/frontend/js/datos.js
@@ -0,0 +1,75 @@
+const infoPaises = {
+    "mexico": {
+        nombre: "MÉXICO",
+        bandera: "🇲🇽",
+        clasificado: "Anfitrión",
+        modelo: "assets/models/huevito_mexico.glb",
+        stats: { jugados: 3, goles: 5, pos: "2°" },
+        descripcion: "El gigante de la CONCACAF busca hacer historia en casa."
+    },
+    "sudafrica": {
+        nombre: "SUDÁFRICA",
+        bandera: "🇿🇦",
+        clasificado: "Clasificado",
+        modelo: "assets/models/huevito.glb",
+        stats: { jugados: 3, goles: 2, pos: "4°" },
+        descripcion: "El equipo africano busca hacer historia en el Mundial."
+    },
+    "corea del sur": {
+        nombre: "COREA DEL SUR",
+        bandera: "🇰🇷",
+        clasificado: "Clasificado",
+        modelo: "assets/models/huevito.glb",
+        stats: { jugados: 3, goles: 4, pos: "1°" },
+        descripcion: "El equipo asiático busca hacer historia en el Mundial."
+    },
+    "tunez": {
+        nombre: "TÚNEZ",
+        bandera: "🇹🇳",
+        clasificado: "Clasificado",
+        modelo: "assets/models/huevito.glb",
+        stats: { jugados: 3, goles: 1, pos: "5°" },
+        descripcion: "El equipo árabe busca hacer historia en el Mundial."
+    },
+    "japon": {
+        nombre: "JAPÓN",
+        bandera: "🇯🇵",
+        clasificado: "Clasificado",
+        modelo: "assets/models/huevito.glb",
+        stats: { jugados: 3, goles: 3, pos: "3°" },
+        descripcion: "El equipo asiático busca hacer historia en el Mundial."
+    },
+    "uzbekistan": {
+        nombre: "UZBEKISTÁN",
+        bandera: "🇺🇿",
+        clasificado: "Clasificado",
+        modelo: "assets/models/huevito.glb",
+        stats: { jugados: 3, goles: 0, pos: "6°" },
+        descripcion: "El equipo asiático busca hacer historia en el Mundial."
+    },
+    "colombia": {
+        nombre: "COLOMBIA",
+        bandera: "🇨🇴",
+        clasificado: "Clasificado",
+        modelo: "assets/models/huevito.glb",
+        stats: { jugados: 3, goles: 2, pos: "4°" },
+        descripcion: "El equipo sudamericano busca hacer historia en el Mundial."
+    },
+    "uruguay": {
+        nombre: "URUGUAY",
+        bandera: "🇺🇾",
+        clasificado: "Clasificado",
+        modelo: "assets/models/huevito.glb",
+        stats: { jugados: 3, goles: 1, pos: "5°" },
+        descripcion: "El equipo sudamericano busca hacer historia en el Mundial."
+    },
+    "españa": {
+        nombre: "ESPAÑA",
+        bandera: "🇪🇸",
+        clasificado: "Clasificado",
+        modelo: "assets/models/huevito.glb",
+        stats: { jugados: 3, goles: 3, pos: "2°" },
+        descripcion: "El equipo europeo busca hacer historia en el Mundial."
+    }
+
+};
